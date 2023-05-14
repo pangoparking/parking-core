e8a793f043f48eda628373fa44bbff26337a7b9b
Kate  D <katebox85@gmail.com>
2023-05-14 22:32:59 +0300

    Renamed FinesRepository to FinesRedisRepository

--- validator/src/main/java/org/parking/service/ValidatorServiceImpl.java
+++ validator/src/main/java/org/parking/service/ValidatorServiceImpl.java
@@ -3,7 +3,7 @@ package org.parking.service;
 import java.time.LocalDateTime;
 
 import org.parking.FineRedisEntity;
-import org.parking.FinesRepository;
+import org.parking.FinesRedisRepository;
 import org.parking.model.CarData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,7 +16,7 @@ import lombok.extern.log4j.Log4j2;
 public class ValidatorServiceImpl implements ValidatorService {
     
 	@Autowired
-	FinesRepository repository;
+	FinesRedisRepository repository;
 	
 	@Override
 	@Transactional

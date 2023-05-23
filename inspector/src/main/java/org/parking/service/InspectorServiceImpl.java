605e80e7acdd1a7335ab1e996ec1407f16370da6
Kate  D <katebox85@gmail.com>
2023-05-23 12:34:01 +0300

    * Added test to inspector

--- inspector/src/main/java/org/parking/service/InspectorServiceImpl.java
+++ inspector/src/main/java/org/parking/service/InspectorServiceImpl.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import lombok.extern.log4j.Log4j2;
@@ -19,35 +20,60 @@ import lombok.extern.log4j.Log4j2;
 @Service
 public class InspectorServiceImpl implements InspectorService {
 	
-	
+
 	@Autowired
 	CarsOnParkingRepository repository;
-	
+
 	@Autowired
 	Environment env;
+	
+//	private final CarsOnParkingRepository repository;
+//    private final Environment env;
+    private final RestTemplate restTemplate;
+    
+    @Autowired
+    public InspectorServiceImpl( RestTemplate restTemplate) {
+     //   this.repository = repository;
+   //     this.env = env;
+       this.restTemplate = restTemplate;
+    }
 	
 	@Override
 	@Transactional
 	public EnumStatus isPangoPayed(CarData car) {
 
-		RestTemplate restTemplate = new RestTemplate();
-		EnumStatus status = EnumStatus.canceled;
+		//RestTemplate restTemplate = new RestTemplate();
 		
-		ParkingTime responseFromPango = restTemplate.getForObject(
-			String.format("http://localhost:%s/car_check/%d",env.getProperty("server.port"),car.carID), ParkingTime.class);
+		EnumStatus status = null;
 		
-        CarRedisEntity carRedis = new CarRedisEntity();
-
-		
-		status = (responseFromPango.parkingEndTime != null && 
-				responseFromPango.parkingEndTime.compareTo(LocalDateTime.now()) > 0) 
-				? EnumStatus.paid : EnumStatus.unpaid;
-		if (status == EnumStatus.paid) {
-			repository.save(carRedis);
-			log.trace("Sent data to Redis: carID %d %s", carRedis.carID, carRedis.parkingEndTime.toString());
+		ParkingTime responseFromPango;
+		CarRedisEntity carRedis = new CarRedisEntity();
+		try {
+			responseFromPango = restTemplate.getForObject(
+				String.format("http://localhost:%s/car_check/%d",env.getProperty("server.port"),car.carID), ParkingTime.class);
+			
+			status = (responseFromPango.parkingEndTime != null && 
+					responseFromPango.parkingEndTime.compareTo(LocalDateTime.now()) > 0) 
+					? EnumStatus.paid : EnumStatus.unpaid;
+			carRedis.carID=car.carID;
+			carRedis.parkingEndTime=responseFromPango.parkingEndTime;
+			
+		} catch (RestClientException e) {
+			log.error("Response from Pango service FAILED",e.getMessage());
 		}
 		
+		if (status == EnumStatus.paid) {
+			try {
+				repository.save(carRedis);
+				log.trace("Sent data to Redis: carID %d %s", carRedis.carID, carRedis.parkingEndTime.toString());
+			} catch (Exception e) {
+				log.error("Sent data to Redis FAILED: carID %d %s", carRedis.carID, carRedis.parkingEndTime.toString(),e.getMessage());
+			}
+		}
+		
+	
 	return status;
+		
 	}
 
 	}
--- inspector/src/test/java/org/parking/InspectorServiceTest.java
+++ inspector/src/test/java/org/parking/InspectorServiceTest.java
@@ -1,42 +1,99 @@
 package org.parking;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.time.LocalDateTime;
-import java.util.Optional;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.parking.model.CarData;
 import org.parking.model.EnumStatus;
+import org.parking.model.ParkingTime;
 import org.parking.service.InspectorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.core.env.Environment;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootTest
+//@Import(TestChannelBinderConfiguration.class)
 public class InspectorServiceTest {
+	
+	//@Autowired
+	//InputDestination producer;
+	
+	@MockBean
+    private RestTemplate restTemplate;
+	
+	 @MockBean
+	 CarsOnParkingRepository repository;
+
+	@Autowired
+	Environment env;
+	 
+	 @Autowired 
+     InspectorService serviceInspector;
+
 	 public long CAR_ID_TIME_OUT = 222;
 	 public long CAR_ID_TIME_PREPAID = 555;
 	 public long PARKING_ID = 67;
 	 public LocalDateTime TIME_OUT=LocalDateTime.now().minusHours(1);
 	 public LocalDateTime TIME_PREPAID=LocalDateTime.now().plusHours(1);
-	 
 	
 	
-	@Autowired
-	InspectorService inspectorService;
-	  
+//	 private static final String BINDING_NAME_VALIDATOR = "inspectorConsumer-in-0";
+		
+ 	
+		
+		@Test
+	    public void InspectorServiceTest_ParkingPrepaid() {
+	        CarData carData = new CarData(CAR_ID_TIME_PREPAID,PARKING_ID);
 
-	//@Test
-	void InspectorService_PangoTimeOut() {
-		var carData = new CarData(CAR_ID_TIME_OUT, PARKING_ID);
-		var serviceResponce = inspectorService.isPangoPayed(carData);
-		assertEquals(serviceResponce,EnumStatus.unpaid);
-	}
+	       ParkingTime responseFromPango = new ParkingTime();
+	       responseFromPango.parkingEndTime= TIME_PREPAID;
+	       responseFromPango.carID=CAR_ID_TIME_PREPAID;
+	       
+	        when(restTemplate.getForObject(any(String.class), any())).thenReturn(responseFromPango);
 
+	        CarRedisEntity carRedis = new CarRedisEntity(responseFromPango.carID,responseFromPango.parkingEndTime);
 
+	        when(repository.save(any(CarRedisEntity.class))).thenReturn(carRedis);
+ 
+	        
+	        
+	        EnumStatus status = serviceInspector.isPangoPayed(carData);
+	        
+            assertEquals(EnumStatus.paid, status);
+	    }
 	
+	
+		@Test
+	    public void InspectorServiceTest_ParkingUnpaid() {
+	        CarData carData = new CarData(CAR_ID_TIME_OUT,PARKING_ID);
+
+	       ParkingTime responseFromPango = new ParkingTime();
+	       responseFromPango.parkingEndTime= TIME_OUT;
+	       responseFromPango.carID=CAR_ID_TIME_OUT;
+	       
+	        when(restTemplate.getForObject(any(String.class), any())).thenReturn(responseFromPango);
+
+	        CarRedisEntity carRedis = new CarRedisEntity(responseFromPango.carID,responseFromPango.parkingEndTime);
+
+	        when(repository.save(any(CarRedisEntity.class))).thenReturn(carRedis);
+ 
+	        
+	        
+	        EnumStatus status = serviceInspector.isPangoPayed(carData);
+	        
+            assertEquals(EnumStatus.unpaid, status);
+	    }
+		
+	
+		
+		
 }
+
